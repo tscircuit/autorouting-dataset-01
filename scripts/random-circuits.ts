f1d043f3b74fd5699f997dece618871a58958881
0hm☘️ <git8ankanroy@gmail.com>
2026-01-27 23:12:26 +0530

    fix: update description in random-circuits script to clarify functionality

--- scripts/random-circuits.ts
+++ scripts/random-circuits.ts
@@ -4,7 +4,7 @@ import { GenerationContext } from "types/GenerationContext"
 
 /**
  * Script to generate a dataset of random circuits.
- * This is used for stress testing or creating varied routing challenges.
+ * Its slides the components on the board and creates random connections between them.
  * It uses the default configuration to generate a set of random circuits.
  */
 const main = async () => {
