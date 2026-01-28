91f66c7e47e372bdb192ce4bebc334eadae8abca
0hm☘️ <git8ankanroy@gmail.com>
2026-01-28 09:38:44 +0530

    Remove randomly generated circuit files 294 to 299 from the project

--- lib/circuit127.tsx
+++
@@ -1,2 +0,0 @@
-/** Randomly generated circuit 127. */
-export default () => <board width="10.00mm" height="10.00mm"></board>
--- lib/circuit129.tsx
+++
@@ -1,2 +0,0 @@
-/** Randomly generated circuit 129. */
-export default () => <board width="12.00mm" height="9.00mm"></board>
