8519cdc469abef59b2b4a0baac9e6f18b4ea602a
0hm☘️ <git8ankanroy@gmail.com>
2026-03-23 10:49:05 +0530

    added check to make sure no pointToConnect is outside

---
+++ scripts/validate-srj/isPointsToConnectInsideObstacle.ts
@@ -0,0 +1,24 @@
+import { getBoundFromCenteredRect } from "@tscircuit/math-utils"
+import { mapLayerNameToZ } from "lib/layer/mapLayerNameToZ"
+import { Obstacle, SimpleRouteConnection } from "tscircuit"
+
+export const isPointsToConnectInsideObstacle = (
+  obstacles: Obstacle[],
+  pointsToConnect: SimpleRouteConnection["pointsToConnect"],
+  layerCount: number
+): boolean => {
+  for (const point of pointsToConnect) {
+      const insideObstacle = obstacles.some((obstacle) => {
+        const layersOfObstacle = obstacle.layers.map((layer) => mapLayerNameToZ(layer, layerCount))
+        const layerOfPoints = mapLayerNameToZ(point.layer, layerCount)
+        const doseSharedLayers = layersOfObstacle.includes(layerOfPoints)
+        if (!doseSharedLayers) return false
+        const { minX, minY, maxX, maxY } = getBoundFromCenteredRect(obstacle)
+        return point.x >= minX && point.x <= maxX && point.y >= minY && point.y <= maxY
+      })
+      if (!insideObstacle) {
+        return false
+      }
+  }
+  return true
+}
