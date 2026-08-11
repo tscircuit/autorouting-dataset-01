5484f97cd20bee746fd27f97b5b148f06de7e689
AnasSarkiz <anassoftwaredev@gmail.com>
2026-08-11 16:07:20 +0200

    fix: keep circuit101 routing DRC clean

--- lib/circuit/circuit101.tsx
+++ lib/circuit/circuit101.tsx
@@ -257,8 +257,8 @@ export default () => (
     <pinheader
       name="pinhead-1"
       footprint="pinrow4"
-      pcbX={-6.5}
-      pcbY={14.0}
+      pcbX={-5.5}
+      pcbY={16.0}
       pcbRotation={0.0}
       layer="top"
       connections={{
